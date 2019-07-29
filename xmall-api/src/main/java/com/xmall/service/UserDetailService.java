776966acd6c6d13a0cee30cf184d983e85994416
pimithie <xq262762708@aliyun.com>
2019-07-29 23:45:20 +0800

    define the api of UserDetailService

---
+++ xmall-api/src/main/java/com/xmall/service/UserDetailService.java
@@ -0,0 +1,33 @@
+package com.xmall.service;
+
+import com.xmall.bean.Message;
+import com.xmall.entity.UserDetail;
+
+/**
+ * @author xiaqi
+ * @date 2019/7/29
+ */
+public interface UserDetailService {
+
+    /**
+     * 根据userId获取用户详细信息
+     * @param userId 指定的userId
+     * @return 响应实体
+     */
+    Message getUserDetailInfoById(Long userId);
+
+    /**
+     * 新增用户详细信息
+     * @param userDetail 新增的用户详细信息userDetail
+     * @return 响应实体
+     */
+    Message insert(UserDetail userDetail);
+
+    /**
+     * 更新用户详细信息
+     * @param userDetail 更新的用户详细信息userDetail
+     * @return 响应实体
+     */
+    Message update(UserDetail userDetail);
+
+}
